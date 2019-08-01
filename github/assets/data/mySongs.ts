f722b39f26fd85e8e70953576c50bddd8f141b2b
Srivasri16 <32154432+Srivasri16@users.noreply.github.com>
2019-08-01 13:57:00 -0400

    Add files via upload

---
+++ github/assets/data/mySongs.ts
@@ -0,0 +1,27 @@
+import {Songs} from '../../app/songInfo';
+
+export const mysongs: Songs[]=
+[{
+    songname:'Nobody has to know',
+    artist:'Kranium',
+    genre:'hip-hop',
+    year:'2014'
+},
+{
+    songname:'WIN',
+    artist:'Jay Rock',
+    genre:'hip-hop',
+    year:'2010'  
+},
+{
+    songname:'Zeus',
+    artist:'Croosh',
+    genre:'hip-hop',
+    year:'2017'  
+},
+{
+    songname:'Toast',
+    artist:'Koffee',
+    genre:'Afro',
+    year:'2008'  
+}]
